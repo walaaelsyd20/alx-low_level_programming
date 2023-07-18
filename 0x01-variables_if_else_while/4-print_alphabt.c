a0e410d992fdc233e7955b2bde12d44e200cd826
Walaa <walaaelsyd20@gmail.com>
2023-07-17 19:22:14 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			ch++;
+		}
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
