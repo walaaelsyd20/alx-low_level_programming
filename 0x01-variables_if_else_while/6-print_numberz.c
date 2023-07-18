77d3807a8a133dc20255b16bd18c3c9ea513c62a
Walaa <walaaelsyd20@gmail.com>
2023-07-17 19:45:04 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints 0-9 using putchar
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
